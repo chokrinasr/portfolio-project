dd7527a76b60b1e78864e7462ad105a829ce86a6
chokrinasr <chokri.nasr1985@mail.com>
2021-12-09 16:49:06 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../logo.gif';
 import "../App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
@@ -8,7 +10,7 @@ const Navbar = () => {
             <div className="container">
   <a className="navbar-brand" href="#"><img src={logo} className="logo" width="48" height="48" alt="" />CHOKRI NASR</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color:"#fff"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
